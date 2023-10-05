25621cceef2b15b3c214591bbb2008c9007b50cf
OnlyDebugAlerts <d.gontarenko.fl@gmail.com>
2023-10-05 11:33:40 +0200

    update token

--- index.js
+++ index.js
@@ -93,4 +93,4 @@ async function playNextSong(message) {
 
 
 
-client.login('MTEzNjMxMTg0MDg2Mzk0ODgwMQ.GaAjR_.NvlvSbvkFOqxh7CKAWZmCraDBJV71BnR_GnDnE');
+client.login('MTEzNjMxMTg0MDg2Mzk0ODgwMQ.G_j9B3.rmyJQTuDHp6aXcbp-aJ--eEIuCFypEdHQYdCvM');
